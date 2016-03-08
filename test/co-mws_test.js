76ca804960934bdaa20fecad765e9722c7aa21d4
Andrea Parodi <andrea@parro.it>
2016-03-08 23:00:06 +0100

    removed mocha add tape

--- test/co-mws_test.js
+++ test/co-mws_test.js
@@ -1,189 +1,184 @@
 'use strict';
 
-import CoMws from '../src';
+const CoMws = require('../src');
+const co = require('co');
+const test = require('tape');
 
-describe('CoMws', () => {
+const setupMws = co.wrap(function *(){
+  const  mws = new CoMws();
+  const ctx = {result:'yet another'};
 
-    it('is defined', () => {
-        CoMws.should.be.a('function');
+  mws.use(function *(next){
+    this.result += yield Promise.resolve(' hello');
+    yield next();
+  });
+
+  mws.use(function *(next){
+    this.result += yield Promise.resolve(' world');
+    yield next();
+  });
+
+  yield mws.run(ctx);
+
+  return {mws, ctx}
+});
+
+const setupErrorMws = co.wrap(function *(){
+  const  mws = new CoMws();
+  mws.use(function *(next){
+    this.result = ('hello');
+    yield next();
+  });
+
+  mws.use(function *(next){
+    throw new Error('test-error');
+  });
+
+  mws.use(function *(ctx, err, next){
+    ctx.ex = err;
+  });
+
+  const ctx = {};
+
+  yield mws.run(ctx);
+
+  return {mws,ctx};
+});
+
+test('register all mws', t => {
+  setupMws().then(ctx => {
+    t.equal(ctx.mws.mws.length, 2);
+    t.end();
+  });
+});
+
+test('can construct instance', t => {
+  setupMws().then(ctx => {
+    t.equal(typeof ctx.mws, 'object');
+    t.end();
+  });
+});
+
+test('execute all mws', t => {
+  setupMws().then(ctx => {
+    t.equal(ctx.ctx.result, 'yet another hello world');
+    t.end();
+  });
+});
+
+test('handle errors', t => {
+  setupErrorMws().then(ctx => {
+    t.equal(ctx.ctx.ex.message, 'test-error');
+    t.end();
+  });
+});
+
+test('allow function mw returning next', t => {
+  let mws;
+  let ctx = {};
+
+  co( function *() {
+    mws = new CoMws();
+
+    mws.use(function(next) {
+      this.result = 'hello';
+      return next();
     });
 
-    describe('is a class', () => {
-        let mws;
-        let ctx = {result:'yet another'};
+    mws.use(function *(next){
+       this.result += ' world';
+    });
 
-        before( function *(){
-            mws = new CoMws();
-            
-            mws.use(function *(next){
-                this.result += yield Promise.resolve(' hello');
-                yield next();
-            });
+    yield mws.run(ctx);
 
-            mws.use(function *(next){
-                this.result += yield Promise.resolve(' world');
-                yield next();
-            });
+  }).then(() => {
+    t.equal(ctx.result, 'hello world');
+    t.end();
+  });
+});
 
-            yield mws.run(ctx);
+test('allow function mw returning a promise', t => {
+  let mws;
+  let ctx = {};
+
+  co( function *() {
+    mws = new CoMws();
+
+    mws.use(function(next) {
+      return new Promise((resolve) => {
+        next().then(()=>{
+          this.result += ' world';
+          resolve();
         });
-        
-        it('register all mws', () => {
-            (mws.mws.length+'').should.be.equal('2');
-        });  
-
-        it('can construct instance', () => {
-            mws.should.be.a('object');
-        });        
-
-        it('execute all mws', () => {
-            ctx.result.should.be.equal('yet another hello world');
-        });        
+      });
     });
 
-    describe('handle errors', () => {
-        let mws;
-        let ex;
-
-        before( function *() {
-            mws = new CoMws();
-            
-            mws.use(function *(next){
-                this.result = ('hello');
-                yield next();
-            });
-
-            mws.use(function *(next){
-                throw new Error('test-error');
-            });
-
-            mws.use(function *(ctx, err, next){
-                ex = err;
-            });
-
-            yield mws.run({});    
-            
-        });
-        
-        it(' - work', () => {
-            ex.message.should.be.equal('test-error');
-        });  
+    mws.use(function *(next){
+       this.result = 'hello';
     });
 
-    describe('allow function mw returning next', () => {
-        let mws;
-        let ctx = {};
-
-        before( function *() {
-            mws = new CoMws();
-            
-            mws.use(function(next) {
-                this.result = 'hello';
-                return next();
-            });
-
-            mws.use(function *(next){
-                 this.result += ' world';
-            });
-
-            yield mws.run(ctx);    
-            
-        });
-        
-        it(' - work', () => {
-            ctx.result.should.be.equal('hello world');
-        });  
-    });
-
-     describe('allow function mw returning a promise', () => {
-        let mws;
-        let ctx = {};
-
-        before( function *() {
-            mws = new CoMws();
-            
-            mws.use(function(next) {
-                return new Promise((resolve) => {
-                    next().then(()=>{
-                        this.result += ' world';    
-                        resolve();
-                    });
-                    
-                });
-                
-            });
-
-            mws.use(function *(next){
-                 this.result = 'hello';
-            });
-
-            yield mws.run(ctx);    
-            
-        });
-        
-        it(' - work', () => {
-            ctx.result.should.be.equal('hello world');
-        });  
-    });
-
-
-     describe('allow function mw returning a normal value', () => {
-        let mws;
-        let ctx = {};
-
-        before( function *() {
-            mws = new CoMws();
-            
-            mws.use(function * (next){
-                this.result = yield Promise.resolve('hello');
-                yield next();
-            });
-
-            mws.use(function (next){
-                this.result += ' world';
-                return null;
-            });
-
-
-            yield mws.run(ctx);    
-            
-        });
-        
-        it(' - work', () => {
-            ctx.result.should.be.equal('hello world');
-        });  
-    });
-
-    describe('allow generators, arrow and normal function with ctx arg', () => {
-        let mws;
-        let ctx = {};
-
-        before( function *() {
-            mws = new CoMws();
-            
-            mws.use((ctx, next) => {
-                ctx.result = 'hello';
-                return next();
-            });
-
-            mws.use(function (ctx, next){
-                ctx.result += ' magic';
-                return next();
-            });
-
-            mws.use(function (ctx, next){
-                ctx.result += ' world';
-            });
-
-
-            yield mws.run(ctx);    
-            
-        });
-        
-        it(' - work', () => {
-            ctx.result.should.be.equal('hello magic world');
-        });  
-    });
-
+    yield mws.run(ctx);
+  }).then(() => {
+    t.equal(ctx.result, 'hello world');
+    t.end();
+  });
 
 });
+
+test('allow function mw returning a normal value', t => {
+  let mws;
+  let ctx = {};
+
+  co( function *() {
+    mws = new CoMws();
+
+    mws.use(function * (next){
+      this.result = yield Promise.resolve('hello');
+      yield next();
+    });
+
+    mws.use(function (next){
+      this.result += ' world';
+      return null;
+    });
+
+
+    yield mws.run(ctx);
+
+  }).then(() => {
+    t.equal(ctx.result, 'hello world');
+    t.end();
+  });
+});
+
+test('allow generators, arrow and normal function with ctx arg', t => {
+  let mws;
+  let ctx = {};
+
+  co( function *() {
+    mws = new CoMws();
+
+    mws.use((ctx, next) => {
+      ctx.result = 'hello';
+      return next();
+    });
+
+    mws.use(function (ctx, next){
+      ctx.result += ' magic';
+      return next();
+    });
+
+    mws.use(function (ctx, next){
+      ctx.result += ' world';
+    });
+
+
+    yield mws.run(ctx);
+
+  }).then(() => {
+    t.equal(ctx.result, 'hello magic world');
+    t.end();
+  });
+});
+
+
